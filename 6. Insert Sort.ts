c151ae231486968a8bf93da0543ca3a3621c6831
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-01-02 23:06:35 +0330

    6. Insert Sort Created.

---
+++ 6. Insert Sort.ts
@@ -0,0 +1,20 @@
+let a6:number[] = [1, 3, 4, 6, 8]
+let n6:number = a6.length
+// let key6:number = -7
+let key6:number = 5
+
+n6 = insert(a6, key6, n6)
+
+function insert(a:number[], key:number, n:number):number{
+    let i:number
+
+    for(i = n-1; (i>=0 && a[i]>key); i--)
+        a[i+1]=a[i]
+    
+    a[i+1]=key
+
+    return (n+1)
+}
+
+for(let i:number = 0; i<n6; i++)
+    console.log(a6[i])
