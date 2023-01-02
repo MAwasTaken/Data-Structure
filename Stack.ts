e13c2d465f93919d4897ce7d7c728a75de7c1f7a
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-01-02 18:20:06 +0330

    Stack.ts cleaned.

--- Stack.ts
+++ Stack.ts
@@ -2,23 +2,27 @@ class Stack<T> {
   private array: T[] = [];
 
   public pop(): T | undefined {
-    if (this.isEmpty()) throw new Error('Empty Stack Exception');
+    if (this.isEmpty()){
+      throw new Error('Empty Stack Exception')
+    }
 
-    return this.array.pop();
+    return this.array!.pop();
   }
 
   public push(data: T): void {
-    this.array.push(data);
+    this.array[this.array.length] = data
   }
 
   public peek(): T {
-    if (this.isEmpty()) throw new Error('Empty Stack Exception');
+    if (this.isEmpty()){
+      throw new Error('Empty Stack Exception')
+    } 
 
-    return this.array[this.array.length - 1];
+    return this.array[this.array.length - 1]
   }
 
   public isEmpty(): boolean {
-    return this.array.length === 0;
+    return this.array.length === 0
   }
 }
 
@@ -31,6 +35,6 @@ stack.push({ 'Name': 'Unknown' })
 
 // console.log(stack.isEmpty())
 // console.log(stack.peek())
-// console.log(stack.pop())
+console.log(stack.pop())
 
 console.log(stack)
